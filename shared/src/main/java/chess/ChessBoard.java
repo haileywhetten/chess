ea4d9a86153c46befd28bbe87cfdd395e687a859
Hailey Whetten <hailwhet@byu.edu>
2025-09-16 13:06:23 -0600

    condensed code for queen moves

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -8,6 +11,21 @@ package chess;
  */
 public class ChessBoard {
     private ChessPiece[][] board = new ChessPiece[8][8];
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(board, that.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(board);
+    }
+
     public ChessBoard() {
         
     }
