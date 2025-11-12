b41ce2f6572038911a67c22015e88785ef252fc0
Hailey Whetten <hailwhet@byu.edu>
2025-11-12 10:54:52 -0700

    Now have command line input in main

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,10 +1,21 @@
 import server.Server;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Server server = new Server();
         server.run(8080);
 
         System.out.println("♕ 240 Chess Server");
+
+        while (true) {
+            System.out.printf("Type your numbers%n>>> ");
+            Scanner scanner = new Scanner(System.in);
+            String line = scanner.nextLine();
+            var numbers = line.split(" ");
+            System.out.println(Arrays.toString(numbers));
+        }
     }
 }
