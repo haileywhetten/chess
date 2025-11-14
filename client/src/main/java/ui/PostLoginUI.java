8b68a5ba3e9709ba813c8cc88afdc1bf70b4e677
Hailey Whetten <hailwhet@byu.edu>
2025-11-13 22:04:41 -0700

    List games and create game work

--- client/src/main/java/ui/PostLoginUI.java
+++ client/src/main/java/ui/PostLoginUI.java
@@ -4,11 +4,13 @@ import model.*;
 import serverfacade.ServerFacade;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class PostLoginUI {
     private final ServerFacade facade;
-    private AuthData auth;
+    private final AuthData auth;
+    private List<GameInfo> gameList = null;
 
     public PostLoginUI(ServerFacade facade, AuthData authData) {
         this.facade = facade;
@@ -26,6 +28,9 @@ public class PostLoginUI {
                 result = eval(line);
                 if(result.equals("preLogin")) {
                     new PreLoginUI(facade).run();
+                }
+                if(result.equals("gameplay")) {
+                    System.out.println("this is where the gameplay ui will go");
                 }
             } catch (Throwable ex) {
                 var msg = ex.toString();
@@ -58,10 +63,10 @@ public class PostLoginUI {
                 case "quit" -> "quit";
                 case "help" -> help();
                 case "logout" -> logout();
-                case "create" -> "create";
+                case "create" -> createGame(params);
                 case "join" -> "join";
                 case "observe" -> "observe";
-                case "list" -> "list";
+                case "list" -> listGames();
                 default -> help();
             };
         } catch (Exception ex) {
@@ -80,6 +85,44 @@ public class PostLoginUI {
     }
 
     public String createGame(String... params) throws Exception {
+        try{
+            if (params.length == 0) {
+                System.out.println(EscapeSequences.SET_TEXT_COLOR_GREEN + "Please enter a name to call your game:");
+                Scanner scanner = new Scanner(System.in);
+                String line = scanner.nextLine();
+                String[] newInfo = line.toLowerCase().split(" ");
+                if (newInfo.length >= 1) {
+                    facade.createGame(newInfo[0], auth);
+                    System.out.printf("%sYou successfully created the game %s.%n", EscapeSequences.SET_TEXT_COLOR_MAGENTA, newInfo[0]);
+                    return "create";
+                }
+            }
+            else {
+                facade.createGame(params[0], auth);
+                System.out.printf("%sYou successfully created the game %s.%n", EscapeSequences.SET_TEXT_COLOR_MAGENTA, params[0]);
+                return "create";
+            }
+        } catch(Exception ex) {
+            throw new Exception("Create game failed.");
+        }
         return null;
+    }
+
+    public String listGames() throws Exception {
+        try {
+            gameList = facade.listGames(auth);
+            if(gameList.isEmpty()) {
+                System.out.printf("%sThere are no games to list :( try creating one!%n", EscapeSequences.SET_TEXT_COLOR_RED);
+            }
+            else {
+                System.out.printf("%sHere is the list of games: %n", EscapeSequences.SET_TEXT_COLOR_RED);
+                for (GameInfo game : gameList) {
+                    System.out.printf("%s%d - %s%n", EscapeSequences.SET_TEXT_COLOR_BLUE, gameList.indexOf(game) + 1, game.gameName());
+                }
+            }
+            return "list";
+        } catch(Exception ex) {
+            throw new Exception("Could not list the games");
+        }
     }
 }
